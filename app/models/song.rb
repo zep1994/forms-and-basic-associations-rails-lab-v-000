b48114400cc31f724469cb135af0e2b4662e155a
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-01 22:24:50 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,18 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist 
+  belongs_to :genre 
+  has_many :notes 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    
+  end
 end
